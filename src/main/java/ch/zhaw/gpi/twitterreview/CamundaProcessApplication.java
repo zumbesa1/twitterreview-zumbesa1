f05aaa4167d843c4e0380f1092702bf16484fed4
Salahudin Zumberi <zumbesa1@students.zhaw.ch>
2019-10-16 13:38:34 +0200

    SW05 fertig

--- src/main/java/ch/zhaw/gpi/twitterreview/CamundaProcessApplication.java
+++ src/main/java/ch/zhaw/gpi/twitterreview/CamundaProcessApplication.java
@@ -1,4 +1,4 @@
-package ch.zhaw.gpi.csbtemplate;
+package ch.zhaw.gpi.twitterreview;
 
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
